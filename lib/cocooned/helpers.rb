bac96a0b7879a80f31d18a94629a187dd03b35da
Gaël-Ian <gael-ian@notus.sh>
2018-08-17 18:17:21 +0200

    Update README and documentation

--- lib/cocooned/helpers.rb
+++ lib/cocooned/helpers.rb
@@ -32,6 +32,20 @@ module Cocooned
     #   cocooned_add_item_link(form, association, options = {})
     #     # Use default name
     #
+    # ==== Parameters
+    #
+    # `label` is the text to be used as the link label.
+    # Just as when you use the Rails builtin helper +link_to+, you can give an explicit
+    # label to your link or use a block to build it.
+    # If you provide neither an explicit label nor a block, Cocooned will try to use I18n
+    # to find a suitable label. This lookup will check the following keys:
+    #
+    # - `cocooned.{association}.add`
+    # - `cocooned.defaults.add`
+    #
+    # If none of these translation exist, it will fallback to 'Add'.
+    #
+    #
     # `form` is your form builder. Can be a SimpleForm::Builder, Formtastic::Builder
     # or a standard Rails FormBuilder.
     #
@@ -48,18 +62,36 @@ module Cocooned
     #   Defaults to `{association.singular_name}_fields`.
     # - **form_name**: name used to access the form builder in the nested form partial.
     #   Defaults to `:f`.
+    # - **form_options**: options to be passed to the nested form builder. Can be used
+    #   to specify a wrapper for simple_form_fields if you use its Bootstrap setup, for
+    #   example.
+    #   No defaults.
     # - **locals**: a hash of local variables, will be forwarded to the partial.
     #   No default.
     #
     # Association options:
     #
-    # - **count**: how many item will be inserted on clic.
+    # - **insertion_method** : the jQuery method to be used to insert new items.
+    #   Can be any of `before`, `after`, `append`, `prepend`, `replaceWith`.
+    #   Defaults to `before`
+    # - **insertion_traversal** and **insertion_node** : respectively the jQuery
+    #   traversal method and the jQuery compatible selector that will be used to find
+    #   the insertion node, relative to the generated link.
+    #   When both are specified, `$(addLink).{traversal}({node})` will be used.
+    #   When only **insertion_node** is specified, `$({node})` will be used.
+    #   When only **insertion_traversal** is specified, it will be ignored.
+    #   When none is specified, `$(addLink).parent()` will be used.
+    # - **count**: how many item will be inserted on click.
     #   Defaults to 1.
-    # - **wrap_object**: a proc used to wrap item instance. Can be useful with decorators
-    #   or special initialisations.
+    # - **wrap_object**: anything responding to `call` to be used to wrap the newly build
+    #   item instance. Can be useful with decorators or special initialisations.
+    #   Ex: cocooned_add_item_link "Add an item", form, :items,
+    #         wrap_object: Proc.new { |comment| CommentDecorator.new(comment) })
     #   No default.
     # - **force_non_association_create**: force to build instances of the nested model
     #   outside association (i.e. without calling `build_{association}` or `{association}.build`)
+    #   Can be usefull if, for some specific reason, you need an object to _not_ be created
+    #   on the association, for example if you did not want `after_add` callbacks to be triggered.
     #   Defaults to false.
     #
     # Link HTML options:
@@ -95,7 +127,10 @@ module Cocooned
         data = cocooned_extract_data!(html_options).merge!(
           association: builder.singular_name,
           associations: builder.plural_name,
-          association_insertion_template: CGI.escapeHTML(rendered.to_str).html_safe
+          association_insertion_template: CGI.escapeHTML(rendered.to_str).html_safe,
+          association_insertion_method: nil,
+          association_insertion_node: nil,
+          association_insertion_traversal: nil,
         )
 
         html_options = {
