fd95b5ea93b81a5c708336d6dff072a0227e4a55
Gaël-Ian <gael-ian@notus.sh>
2022-10-31 22:42:02 -0400

    Add tests on associationInsertionTraversal

--- __tests__/unit/addLink/associationInsertionNode.js
+++ __tests__/unit/addLink/associationInsertionNode.js
@@ -27,8 +27,24 @@ describe('A Cocooned setup', () => {
     `);
     given('insertionNode', () => given.link.parentElement);
 
-    it('insert new items at the expected place', () => {
-      expect(given.insertionNode.previousSibling).toBe(given.item);
+    it('insert new items before link parent', () => {
+      expect(given.insertionNode.previousElementSibling).toBe(given.item);
+    });
+  });
+
+  describe('with association-insertion-node as "this"', () => {
+    given('template', () => `
+      <section>
+        <a class="cocooned-add" href="#"
+           data-associations="items"
+           data-association-insertion-node="this"
+           data-association-insertion-template="${asAttribute(given.insertionTemplate)}">Add</a>
+      </section>
+    `);
+    given('insertionNode', () => given.link);
+
+    it('insert new items before the link', () => {
+      expect(given.insertionNode.previousElementSibling).toBe(given.item);
     });
   });
 
@@ -45,8 +61,46 @@ describe('A Cocooned setup', () => {
     `);
     given('insertionNode', () => given.container.querySelector('.insertion-node'));
 
-    it('insert new items at the expected place', () => {
-      expect(given.insertionNode.previousSibling).toBe(given.item);
+    it('insert new items before the expected element', () => {
+      expect(given.insertionNode.previousElementSibling).toBe(given.item);
+    });
+
+    describe('with association-insertion-traversal as a jQuery traversal method name', () => {
+      // See https://api.jquery.com/category/traversing/tree-traversal/
+      // Methods other than those listed here does not really make sense as a viable traversal method.
+      const traversals = [
+        { traversal: 'closest',   finder: (link) => link.closest('.closest') },
+        // Somehow broken :/
+        // Can't use nextElementSibling here as, at the time of finding, it is no more the next one.
+        // { traversal: 'next',      finder: (link) => link.parentElement.querySelector(':scope > .next') },
+        { traversal: 'parent',    finder: (link) => link.parentElement },
+        { traversal: 'prev',      finder: (link) => link.previousElementSibling },
+        { traversal: 'siblings',  finder: (link) => link.parentElement.querySelector(':scope > .siblings') },
+      ];
+
+      given('template', () => `
+        <section>
+          <div class="closest">
+            <div class="parent">
+              <div class="prev siblings"></div>
+              <a class="cocooned-add" href="#"
+                 data-associations="items"
+                 data-association-insertion-node=".${given.insertionTraversal}"
+                 data-association-insertion-traversal="${given.insertionTraversal}"
+                 data-association-insertion-template="${asAttribute(given.insertionTemplate)}">Add</a>
+              <div class="next"></div> 
+            </div>
+          </div>
+        </section>
+      `);
+
+      describe.each(traversals)('when $traversal', ({ traversal, finder }) => {
+        given('insertionTraversal', () => traversal);
+
+        it('insert new items before the expected element', () => {
+          expect(finder(given.link).previousElementSibling).toBe(given.item);
+        });
+      });
     });
   });
 });
