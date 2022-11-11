64329a9c7bf4c646f2c7f89e3b78498838901020
Gaël-Ian <gael-ian@notus.sh>
2022-11-10 19:21:29 -0500

    Add tests on the jQuery integration

---
+++ __tests__/unit/compatibility/jquery.js
@@ -0,0 +1,24 @@
+const Cocooned = require('../../../app/assets/javascripts/cocooned');
+const { asAttribute, clickEvent } = require('../../support/helpers');
+
+describe('A Cocooned setup', () => {
+  given('template', () => `
+    <section>
+      <div>
+        <a class="cocooned-add" href="#"
+           data-association-insertion-template="${asAttribute(given.insertionTemplate)}">Add</a>
+      </div>
+    </section>
+  `);
+  given('insertionTemplate', () => `<div class="cocooned-item"></div>`);
+  given('container', () => document.querySelector('section'));
+
+  beforeEach(() => {
+    document.body.innerHTML = given.template;
+    $(given.container).cocooned();
+  });
+
+  it('is instanciated as a Cocooned instance', () => {
+    expect(given.container).toHaveClass('cocooned-container');
+  });
+});
