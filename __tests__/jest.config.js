ab108bd211fada500e04ccd49ad56691df0785e1
Gaël-Ian <gael-ian@notus.sh>
2022-11-28 22:12:19 -0500

    Fix code coverage configuration for Jest

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -13,14 +13,20 @@ module.exports = {
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/tmp/jest_rs",
 
-  // Automatically clear mock calls, instances, contexts and results before every test
+  // Automatically clear mock calls, instances, contexts and results before
+  // every test
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
+  // Indicates whether the coverage information should be collected while
+  // executing the test
   collectCoverage: true,
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  // An array of glob patterns indicating a set of files for which coverage
+  // information should be collected
+  //collectCoverageFrom: [
+  //  '**/*.js',
+  //  '!jest.config.js',
+  //],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: '../coverage',
@@ -31,7 +37,7 @@ module.exports = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  // coverageProvider: 'babel',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -48,7 +54,7 @@ module.exports = {
   // dependencyExtractor: undefined,
 
   // Make calling deprecated APIs throw helpful error messages
-  // errorOnDeprecated: false,
+  errorOnDeprecated: true,
 
   // The default configuration for fake timers
   // fakeTimers: {
@@ -58,20 +64,25 @@ module.exports = {
   // Force coverage collection from ignored files using an array of glob patterns
   // forceCoverageMatch: [],
 
-  // A path to a module which exports an async function that is triggered once before all test suites
+  // A path to a module which exports an async function that is triggered once
+  // before all test suites
   // globalSetup: undefined,
 
-  // A path to a module which exports an async function that is triggered once after all test suites
+  // A path to a module which exports an async function that is triggered once
+  // after all test suites
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
 
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will
-  // use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  // The maximum amount of workers used to run your tests.
+  // Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of
+  // your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a
+  // maximum of 2 workers.
   // maxWorkers: "50%",
 
-  // An array of directory names to be searched recursively up from the requiring module's location
+  // An array of directory names to be searched recursively up from the requiring
+  // module's location
   // moduleDirectories: [
   //   "node_modules"
   // ],
@@ -88,10 +99,12 @@ module.exports = {
   //   "node"
   // ],
 
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // A map from regular expressions to module names or to arrays of module names
+  // that allow to stub out resources with a single module
   // moduleNameMapper: {},
 
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // An array of regexp pattern strings, matched against all module paths before
+  // considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
@@ -132,20 +145,24 @@ module.exports = {
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
-  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // The paths to modules that run some code to configure or set up the testing
+  // environment before each test
   // setupFiles: [],
 
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // A list of paths to modules that run some code to configure or set up the
+  // testing framework before each test
   setupFilesAfterEnv: [
     'givens/setup.js',
     './support/jquery.js',
     './support/matchers.js'
   ],
 
-  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  // The number of seconds after which a test is considered as slow and reported
+  // as such in the results.
   // slowTestThreshold: 5,
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // A list of paths to snapshot serializer modules Jest should use for snapshot
+  // testing
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
@@ -163,9 +180,11 @@ module.exports = {
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  // An array of regexp pattern strings that are matched against all test paths,
+  // matched tests are skipped
   testPathIgnorePatterns: [
     '/node_modules/',
+    '/app/assets/',
     '/support/',
     '/shared/',
     '/*.config.js'
@@ -183,19 +202,22 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // An array of regexp pattern strings that are matched against all source file
+  // paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/",
   //   "\\.pnp\\.[^\\/]+$"
   // ],
 
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // An array of regexp pattern strings that are matched against all modules
+  // before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
   // verbose: undefined,
 
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+  // An array of regexp patterns that are matched against all source file paths
+  // before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
 
   // Whether to use watchman for file crawling
