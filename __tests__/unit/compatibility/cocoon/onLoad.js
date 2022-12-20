c5384defdb28508f54e88f3dee11746c4bf94584
Gaël-Ian <gael-ian@notus.sh>
2022-12-19 21:46:42 -0500

    Add more compatibility tests

---
+++ __tests__/unit/compatibility/cocoon/onLoad.js
@@ -0,0 +1,27 @@
+/* global given */
+
+const Cocooned = require('@cocooned/src/javascripts/cocooned')
+const { asAttribute } = require('@cocooned/tests/support/helpers')
+
+describe('A Cocoon setup', () => {
+  given('template', () => `
+    <section>
+      <div>
+        <a class="add_fields" href="#"
+           data-association-insertion-template="${asAttribute(given.insertionTemplate)}">Add</a>
+      </div>
+    </section>
+  `)
+  given('insertionTemplate', () => '<div class="cocooned-item"></div>')
+  given('container', () => document.querySelector('section'))
+
+  beforeEach(() => {
+    document.body.innerHTML = given.template
+    window.Cocooned = Cocooned
+    require('../../../../app/assets/javascripts/cocoon')
+  })
+
+  it('is instanciated as a Cocooned instance', () => {
+    expect(given.container).toHaveClass('cocooned-container')
+  })
+})
