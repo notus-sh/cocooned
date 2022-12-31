ec1664192e3cdcb8a786328ccba805cec55a5812
Gaël-Ian <gael-ian@notus.sh>
2022-12-30 20:20:41 -0500

    Rewrite JavaScript Builder to use a DOM DocumentFragment as its source

--- npm/__tests__/unit/cocooned/builder.js
+++ npm/__tests__/unit/cocooned/builder.js
@@ -4,54 +4,39 @@ import Builder from '@notus.sh/cocooned/src/cocooned/builder'
 import { faker } from '@cocooned/tests/support/faker'
 
 describe('Builder', () => {
-  given('builder', () => new Builder(given.template, given.singular, given.plural))
-  given('singular', () => 'person')
-  given('plural', () => 'people')
+  given('builder', () => new Builder(given.template.content, given.association))
+  given('association', () => 'new_person')
   given('id', () => faker.random.numeric(5))
 
   const replacements = [
     {
       desc: 'braced singular',
-      template: '<input type="text" name="contacts[person][name]" />',
-      expected: (id) => `<input type="text" name="contacts[${id}][name]" />`
+      template: '<input type="text" name="contacts[new_person][name]">',
+      expected: (id) => `<input type="text" name="contacts[${id}][name]">`
     },
     {
       desc: 'underscored singular',
-      template: '<input type="text" id="contacts_person_name" />',
-      expected: (id) => `<input type="text" id="contacts_${id}_name" />`
+      template: '<input type="text" id="contacts_new_person_name">',
+      expected: (id) => `<input type="text" id="contacts_${id}_name">`
     },
     {
       desc: 'both singular',
-      template: '<input type="text" id="contacts_person_name" name="contacts[person][name]" />',
-      expected: (id) => `<input type="text" id="contacts_${id}_name" name="contacts[${id}][name]" />`
-    },
-    {
-      desc: 'braced plural',
-      template: '<input type="text" name="contacts[people][name]" />',
-      expected: (id) => `<input type="text" name="contacts[${id}][name]" />`
-    },
-    {
-      desc: 'underscored plural',
-      template: '<input type="text" id="contacts_people_name" />',
-      expected: (id) => `<input type="text" id="contacts_${id}_name" />`
-    },
-    {
-      desc: 'both plural',
-      template: '<input type="text" id="contacts_people_name" name="contacts[people][name]" />',
-      expected: (id) => `<input type="text" id="contacts_${id}_name" name="contacts[${id}][name]" />`
-    },
-    {
-      desc: 'singular and plural',
-      template: '<input type="text" id="contacts_people_name" name="contacts[person][name]" />',
-      expected: (id) => `<input type="text" id="contacts_${id}_name" name="contacts[${id}][name]" />`
+      template: '<input type="text" id="contacts_new_person_name" name="contacts[new_person][name]">',
+      expected: (id) => `<input type="text" id="contacts_${id}_name" name="contacts[${id}][name]">`
     }
   ]
 
   describe.each(replacements)('with $desc occurrences', ({ desc, template, expected }) => {
-    given('template', () => template)
+    beforeEach(() => { document.body.innerHTML = `<template>${template}</template>` })
+
+    given('template', () => document.querySelector('template'))
 
     it('replace occurences', () => {
-      expect(given.builder.build(given.id)).toEqual(expected(given.id))
+      const built = given.builder.build(given.id)
+      const div = document.createElement('div')
+      div.appendChild(built)
+
+      expect(div.innerHTML).toEqual(expected(given.id))
     })
   })
 })
