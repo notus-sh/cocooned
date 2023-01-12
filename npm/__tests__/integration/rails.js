f458829d381862fcdd5ce26c8f4bbbe8c6050eba
Gaël-Ian <gael-ian@notus.sh>
2023-01-11 21:41:44 -0500

    Rename integration tests with Rails

--- npm/__tests__/integration/rails.js
+++ npm/__tests__/integration/rails.js
@@ -7,9 +7,9 @@ import { setup, clickEvent } from '@cocooned/tests/support/helpers'
 import { getAddLink, getRemoveLink, getMoveUpLink, getMoveDownLink } from '@cocooned/tests/support/selectors'
 
 // TODO: When officially supported, replace with:
-// import fixtures from '@cocooned/tests/fixtures/list.json' assert { type: 'json' }
+// import fixtures from '@cocooned/tests/fixtures/rails.json' assert { type: 'json' }
 import { readFile } from 'fs/promises'
-const fixtures = JSON.parse(await readFile(new URL('./../fixtures/list.json', import.meta.url)))
+const fixtures = JSON.parse(await readFile(new URL('./../fixtures/rails.json', import.meta.url)))
 
 describe('A Rails generated Cocooned setup', () => {
   given('template', () => fixtures.template)
