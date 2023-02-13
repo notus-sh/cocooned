fe1d647dd37849e67d6d471b34919f78e4f3d279
Gaël-Ian <gael-ian@notus.sh>
2023-02-13 00:06:56 -0500

    Update changelog

--- npm/cocooned.js
+++ npm/cocooned.js
@@ -3,7 +3,7 @@ import { deprecator } from './src/cocooned/deprecation'
 
 deprecator('3.0').warn(
   'Loading @notus.sh/cocooned/cocooned is deprecated',
-  '@notus.sh/cocooned/jquery, @notus.sh/cocooned or @notus.sh/cocooned/src/base'
+  '@notus.sh/cocooned/jquery, @notus.sh/cocooned or `@notus.sh/cocooned/src/cocooned/cocooned`'
 )
 
 export default Cocooned
