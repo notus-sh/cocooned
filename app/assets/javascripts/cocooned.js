b197ba6c96543d54e432a29b95f26d3b879daedc
Gaël-Ian <gael-ian@users.noreply.github.com>
2024-01-28 14:35:26 +0100

    Prepare release 2.1.0 (#60)
    * Update changelog
    * Bump version to 2.1.0
    * Rebuild test fixtures and JavaScript legacy bundle for old Sprockets

--- app/assets/javascripts/cocooned.js
+++ app/assets/javascripts/cocooned.js
@@ -309,10 +309,7 @@
 
     build (id) {
       const node = this.#documentFragment.cloneNode(true);
-      this.#replacements.forEach(replacement => {
-        node.querySelectorAll(`*[${replacement.attribute}]`).forEach(node => replacement.apply(node, id));
-      });
-
+      this.#applyReplacements(node, id);
       return node
     }
 
@@ -320,6 +317,16 @@
     #association
     #documentFragment
     #replacements
+
+    #applyReplacements(node, id) {
+      this.#replacements.forEach(replacement => {
+        node.querySelectorAll(`*[${replacement.attribute}]`).forEach(node => replacement.apply(node, id));
+      });
+
+      node.querySelectorAll('template').forEach(template => {
+        this.#applyReplacements(template.content, id);
+      });
+    }
   }
 
   class Traverser {
