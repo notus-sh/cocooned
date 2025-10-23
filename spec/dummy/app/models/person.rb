d5245576647bd529cf6fbb5780abb4353d9e896b
Gaël-Ian <gael-ian@notus.sh>
2025-10-23 14:23:38 +0200

    Update Rubocop configuration

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Person < ApplicationRecord
-  has_one :biography, class_name: 'Post'
+  has_one :biography, class_name: 'Post', dependent: :destroy
 
-  has_many :posts, inverse_of: :author
+  has_many :posts, inverse_of: :author, dependent: :destroy
   accepts_nested_attributes_for :posts, reject_if: :all_blank, allow_destroy: true
 
+  # rubocop:disable Rails/HasAndBelongsToMany
   has_and_belongs_to_many :contacts, class_name: 'Person'
   has_and_belongs_to_many :alumni, -> { where status: :student }, class_name: 'Person'
+  # rubocop:enable Rails/HasAndBelongsToMany
 end
