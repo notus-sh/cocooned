d2c500fd1052e94be8156131bc55a75780d9804c
Gaël-Ian <gael-ian@notus.sh>
2026-03-15 19:05:23 +0100

    Rebuild javascript for legacy assets pipelines (Sprockets)

--- app/assets/javascripts/cocooned.js
+++ app/assets/javascripts/cocooned.js
@@ -168,15 +168,15 @@
 
   disposable(Listener);
 
-  if (typeof Symbol.dispose !== "symbol") {
+  if (typeof Symbol.dispose !== 'symbol') {
     console.warn(`
     Cocooned use Disposable objects but they are not supported by your browser.
     See Cocooned documentation for polyfill options.
   `);
   }
 
-  function disposable(klass) {
-    if (typeof Symbol.dispose !== "symbol") {
+  function disposable (klass) {
+    if (typeof Symbol.dispose !== 'symbol') {
       return
     }
 
